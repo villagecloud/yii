e48bf7bafea2d5a7fd892aa743497a8d244cdbdb
serge_ <jumpaper@gmail.com>
2018-12-17 16:19:36 +0300

    no message

---
+++ models/Task.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 17/12/18
+ * Time: 12:53
+ */
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class Task extends Model
+{
+    public $id;
+    public $status;
+}
