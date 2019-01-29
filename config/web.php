85884f2d7a7ea0300ce659ef30e54ad6a0497a7f
serge_ <jumpaper@gmail.com>
2019-01-29 16:46:30 +0300

    Установлен GUI RBAC - mdmsoft

--- config/web.php
+++ config/web.php
@@ -16,6 +16,33 @@ $config = [
         'admin' => [
             'class' => 'app\modules\admin\module',
         ],
+        'rbac' => [
+            'class' => 'mdm\admin\Module',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                    /* 'userClassName' => 'app\models\User', */
+                    'idField' => 'id',
+                    'usernameField' => 'username',
+                ],
+            ],
+            'layout' => 'left-menu',
+            'mainLayout' => '@app/views/layouts/main.php',
+        ],
+    ],
+    'as access' => [
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            'site/*',
+            'admin/*',
+            'rbac/*',
+            'task/index',
+            // The actions listed here will be allowed to everyone including guests.
+            // So, 'admin/*' should not appear here in the production, of course.
+            // But in the earlier stages of your development, you may probably want to
+            // add a lot of actions here until you finally completed setting up rbac,
+            // otherwise you may not even take a first step.
+        ]
     ],
     'components' => [
         'authManager' =>[
